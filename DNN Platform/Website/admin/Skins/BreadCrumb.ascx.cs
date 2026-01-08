0a90355f01cb773b0499f1a544a0918ac5df3a3a
Ali Nazari <52697118+uzmannazari@users.noreply.github.com>
2026-01-08 19:11:40 +0330

    Refactor BreadCrumb class for improved functionality
    Updated the BreadCrumb class to enhance functionality and maintain compatibility with legacy systems. Introduced new properties for cleaner markup and list-based semantic rendering.

--- DNN Platform/Website/admin/Skins/BreadCrumb.ascx.cs
+++ DNN Platform/Website/admin/Skins/BreadCrumb.ascx.cs
@@ -4,21 +4,26 @@
 namespace DotNetNuke.UI.Skins.Controls
 {
     using System;
+    using System.Globalization;
     using System.Text;
     using System.Text.RegularExpressions;
+    using System.Web;
 
     using DotNetNuke.Abstractions;
     using DotNetNuke.Abstractions.Portals;
     using DotNetNuke.Common;
     using DotNetNuke.Common.Utilities;
     using DotNetNuke.Entities.Tabs;
+    using DotNetNuke.Services.Localization;
     using Microsoft.Extensions.DependencyInjection;
 
     /// <summary>A skin/theme object which displays the hierarchy of the current page.</summary>
     public partial class BreadCrumb : SkinObjectBase
     {
+        private const string MyFileName = "BreadCrumb.ascx";
         private const string UrlRegex = "(href|src)=(\\\"|'|)(.[^\\\"']*)(\\\"|'|)";
         private readonly INavigationManager navigationManager;
+
         private string separator = "<img alt=\"breadcrumb separator\" src=\"" + Globals.ApplicationPath + "/images/breadcrumb.gif\">";
         private string cssClass = "SkinObject";
         private int rootLevel;
@@ -30,6 +35,9 @@ namespace DotNetNuke.UI.Skins.Controls
         {
             this.navigationManager = Globals.GetCurrentServiceProvider().GetRequiredService<INavigationManager>();
             this.CleanerMarkup = false;
+
+            // Default to Legacy to avoid breaking changes.
+            this.UseListMarkup = false;
         }
 
         public int ProfileUserId
@@ -52,6 +60,7 @@ namespace DotNetNuke.UI.Skins.Controls
             }
         }
 
+        // Separator between breadcrumb elements
         public string Separator
         {
             get { return this.separator; }
@@ -64,16 +73,18 @@ namespace DotNetNuke.UI.Skins.Controls
             set { this.cssClass = value; }
         }
 
+        // Level to begin processing breadcrumb at.
+        // -1 means show root breadcrumb
         public string RootLevel
         {
             get
             {
-                return this.rootLevel.ToString();
+                return this.rootLevel.ToString(CultureInfo.InvariantCulture);
             }
 
             set
             {
-                this.rootLevel = int.Parse(value);
+                this.rootLevel = int.Parse(value, CultureInfo.InvariantCulture);
                 if (this.rootLevel < 0)
                 {
                     this.showRoot = true;
@@ -82,11 +93,20 @@ namespace DotNetNuke.UI.Skins.Controls
             }
         }
 
+        // Use the page title instead of page name
         public bool UseTitle { get; set; }
 
+        // Do not show when there is no breadcrumb (only has current tab)
         public bool HideWithNoBreadCrumb { get; set; }
 
+        /// <summary>Gets or sets a value indicating whether to take advantage of the enhanced markup (remove extra wrapping elements).</summary>
         public bool CleanerMarkup { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether opt-in: render list-based semantic markup (nav/ol/li).
+        /// Default is false to preserve legacy output and reduce breaking changes.
+        /// </summary>
+        public bool UseListMarkup { get; set; }
 
         private IPortalAliasInfo CurrentPortalAlias => this.PortalSettings.PortalAlias;
 
@@ -94,50 +114,134 @@ namespace DotNetNuke.UI.Skins.Controls
         {
             base.OnLoad(e);
 
-            var position = 1;
-            var breadcrumb = new StringBuilder();
-
             this.ResolveSeparatorPaths();
 
-            if (this.HideWithNoBreadCrumb && this.PortalSettings.ActiveTab.BreadCrumbs.Count == (this.rootLevel + 1))
+            var crumbCount = this.PortalSettings.ActiveTab.BreadCrumbs.Count;
+
+            if (this.HideWithNoBreadCrumb && crumbCount == (this.rootLevel + 1))
             {
                 return;
             }
 
-            var crumbCount = this.PortalSettings.ActiveTab.BreadCrumbs.Count;
-            var listItemIndex = 0;
+            this.lblBreadCrumb.Text = this.UseListMarkup
+                ? this.BuildListMarkup(crumbCount)
+                : this.BuildLegacyMarkup(crumbCount);
+        }
+
+        private string BuildLegacyMarkup(int crumbCount)
+        {
+            var position = 1;
+
+            // IMPORTANT:
+            // We intentionally keep the legacy outer span wrapper so existing skins/CSS don't break.
+            // This mimics the old asp:Label output wrapper and keeps schema breadcrumb attrs.
+            var breadcrumb = new StringBuilder(
+                "<span itemprop=\"breadcrumb\" itemscope itemtype=\"https://schema.org/breadcrumb\">" +
+                "<span itemscope itemtype=\"http://schema.org/BreadcrumbList\">");
+
+            // Without checking if the current tab is the home tab, we would duplicate the root tab
+            if (this.showRoot && this.PortalSettings.ActiveTab.TabID != this.PortalSettings.HomeTabId)
+            {
+                this.ResolveHome(out var homeUrl, out var homeNameEncoded);
+
+                breadcrumb.Append("<span itemprop=\"itemListElement\" itemscope itemtype=\"http://schema.org/ListItem\">");
+                breadcrumb.AppendFormat(
+                    CultureInfo.InvariantCulture,
+                    "<a href=\"{0}\" class=\"{1}\" itemprop=\"item\"><span itemprop=\"name\">{2}</span></a>",
+                    homeUrl,
+                    this.cssClass,
+                    homeNameEncoded);
+                breadcrumb.AppendFormat(
+                    CultureInfo.InvariantCulture,
+                    "<meta itemprop=\"position\" content=\"{0}\" />",
+                    position++);
+                breadcrumb.Append("</span>");
+
+                breadcrumb.Append(this.separator);
+            }
+
+            for (var i = this.rootLevel; i < crumbCount; ++i)
+            {
+                if (i > this.rootLevel)
+                {
+                    breadcrumb.Append(this.separator);
+                }
+
+                var tab = (TabInfo)this.PortalSettings.ActiveTab.BreadCrumbs[i];
+
+                var tabName = this.GetTabDisplayName(tab);
+                var tabNameEncoded = HttpUtility.HtmlEncode(tabName ?? string.Empty);
+
+                var tabUrl = this.GetTabUrl(tab);
+
+                if (tab.DisableLink)
+                {
+                    if (this.CleanerMarkup)
+                    {
+                        breadcrumb.AppendFormat(
+                            CultureInfo.InvariantCulture,
+                            "<span class=\"{0}\">{1}</span>",
+                            this.cssClass,
+                            tabNameEncoded);
+                    }
+                    else
+                    {
+                        breadcrumb.AppendFormat(
+                            CultureInfo.InvariantCulture,
+                            "<span><span class=\"{0}\">{1}</span></span>",
+                            this.cssClass,
+                            tabNameEncoded);
+                    }
+                }
+                else
+                {
+                    breadcrumb.Append("<span itemprop=\"itemListElement\" itemscope itemtype=\"http://schema.org/ListItem\">");
+                    breadcrumb.AppendFormat(
+                        CultureInfo.InvariantCulture,
+                        "<a href=\"{0}\" class=\"{1}\" itemprop=\"item\"><span itemprop=\"name\">{2}</span></a>",
+                        tabUrl,
+                        this.cssClass,
+                        tabNameEncoded);
+                    breadcrumb.AppendFormat(
+                        CultureInfo.InvariantCulture,
+                        "<meta itemprop=\"position\" content=\"{0}\" />",
+                        position++);
+                    breadcrumb.Append("</span>");
+                }
+            }
+
+            // close both wrappers
+            breadcrumb.Append("</span></span>");
+            return breadcrumb.ToString();
+        }
+
+        private string BuildListMarkup(int crumbCount)
+        {
+            var position = 1;
+            var breadcrumb = new StringBuilder();
+
+            // IMPORTANT:
+            // List markup must start with <nav> (no legacy wrapper).
+            breadcrumb.AppendFormat(
+                CultureInfo.InvariantCulture,
+                "<nav class=\"dnnBreadcrumb\" aria-label=\"{0}\"><ol itemscope itemtype=\"https://schema.org/BreadcrumbList\">",
+                HttpUtility.HtmlEncode(this.GetAriaLabel()));
 
             if (this.showRoot && this.PortalSettings.ActiveTab.TabID != this.PortalSettings.HomeTabId)
             {
-                this.homeUrl = Globals.AddHTTP(this.CurrentPortalAlias.HttpAlias);
-
-                if (this.PortalSettings.HomeTabId != -1)
-                {
-                    this.homeUrl = this.navigationManager.NavigateURL(this.PortalSettings.HomeTabId);
-
-                    var tc = new TabController();
-                    var homeTab = tc.GetTab(this.PortalSettings.HomeTabId, this.PortalSettings.PortalId, false);
-                    this.homeTabName = homeTab.LocalizedTabName;
-
-                    if (this.UseTitle && !string.IsNullOrEmpty(homeTab.Title))
-                    {
-                        this.homeTabName = homeTab.Title;
-                    }
-                }
-
-                var hasMoreAfterRoot = crumbCount > this.rootLevel;
+                this.ResolveHome(out var homeUrl, out var homeNameEncoded);
 
                 breadcrumb.Append("<li itemprop=\"itemListElement\" itemscope itemtype=\"https://schema.org/ListItem\">");
-                breadcrumb.Append("<a href=\"" + this.homeUrl + "\" class=\"" + this.cssClass + "\" itemprop=\"item\"><span itemprop=\"name\">" + this.homeTabName + "</span></a>");
-                breadcrumb.Append("<meta itemprop=\"position\" content=\"" + position++ + "\" />");
-
-                if (hasMoreAfterRoot)
-                {
-                    breadcrumb.Append("<span aria-hidden=\"true\">");
-                    breadcrumb.Append(this.separator);
-                    breadcrumb.Append("</span>");
-                }
-
+                breadcrumb.AppendFormat(
+                    CultureInfo.InvariantCulture,
+                    "<a href=\"{0}\" class=\"{1}\" itemprop=\"item\"><span itemprop=\"name\">{2}</span></a>",
+                    homeUrl,
+                    this.cssClass,
+                    homeNameEncoded);
+                breadcrumb.AppendFormat(
+                    CultureInfo.InvariantCulture,
+                    "<meta itemprop=\"position\" content=\"{0}\" />",
+                    position++);
                 breadcrumb.Append("</li>");
             }
 
@@ -145,40 +249,41 @@ namespace DotNetNuke.UI.Skins.Controls
             {
                 var tab = (TabInfo)this.PortalSettings.ActiveTab.BreadCrumbs[i];
 
-                var tabName = tab.LocalizedTabName;
+                var tabName = this.GetTabDisplayName(tab);
+                var tabNameEncoded = HttpUtility.HtmlEncode(tabName ?? string.Empty);
 
-                if (this.UseTitle && !string.IsNullOrEmpty(tab.Title))
-                {
-                    tabName = tab.Title;
-                }
+                var tabUrl = this.GetTabUrl(tab);
 
-                var tabUrl = tab.FullUrl;
-
-                if (this.ProfileUserId > -1)
-                {
-                    tabUrl = this.navigationManager.NavigateURL(tab.TabID, string.Empty, "UserId=" + this.ProfileUserId);
-                }
-
-                if (this.GroupId > -1)
-                {
-                    tabUrl = this.navigationManager.NavigateURL(tab.TabID, string.Empty, "GroupId=" + this.GroupId);
-                }
-
-                var isLastCrumb = i == crumbCount - 1;
+                var isLastCrumb = i == (crumbCount - 1);
                 var liAriaCurrent = isLastCrumb ? " aria-current=\"page\"" : string.Empty;
 
-                breadcrumb.Append("<li itemprop=\"itemListElement\" itemscope itemtype=\"https://schema.org/ListItem\"" + liAriaCurrent + ">");
+                breadcrumb.AppendFormat(
+                    CultureInfo.InvariantCulture,
+                    "<li itemprop=\"itemListElement\" itemscope itemtype=\"https://schema.org/ListItem\"{0}>",
+                    liAriaCurrent);
 
                 if (tab.DisableLink)
                 {
-                    breadcrumb.Append("<span class=\"" + this.cssClass + "\" itemprop=\"name\">" + tabName + "</span>");
+                    breadcrumb.AppendFormat(
+                        CultureInfo.InvariantCulture,
+                        "<span class=\"{0}\" itemprop=\"name\">{1}</span>",
+                        this.cssClass,
+                        tabNameEncoded);
                 }
                 else
                 {
-                    breadcrumb.Append("<a href=\"" + tabUrl + "\" class=\"" + this.cssClass + "\" itemprop=\"item\"><span itemprop=\"name\">" + tabName + "</span></a>");
+                    breadcrumb.AppendFormat(
+                        CultureInfo.InvariantCulture,
+                        "<a href=\"{0}\" class=\"{1}\" itemprop=\"item\"><span itemprop=\"name\">{2}</span></a>",
+                        tabUrl,
+                        this.cssClass,
+                        tabNameEncoded);
                 }
 
-                breadcrumb.Append("<meta itemprop=\"position\" content=\"" + position++ + "\" />");
+                breadcrumb.AppendFormat(
+                    CultureInfo.InvariantCulture,
+                    "<meta itemprop=\"position\" content=\"{0}\" />",
+                    position++);
 
                 if (!isLastCrumb)
                 {
@@ -188,10 +293,74 @@ namespace DotNetNuke.UI.Skins.Controls
                 }
 
                 breadcrumb.Append("</li>");
-                listItemIndex++;
             }
 
-            this.lblBreadCrumb.Text = breadcrumb.ToString();
+            breadcrumb.Append("</ol></nav>");
+            return breadcrumb.ToString();
+        }
+
+        private void ResolveHome(out string homeUrl, out string homeNameEncoded)
+        {
+            homeUrl = Globals.AddHTTP(this.CurrentPortalAlias.HttpAlias);
+            var homeName = this.homeTabName;
+
+            if (this.PortalSettings.HomeTabId != -1)
+            {
+                homeUrl = this.navigationManager.NavigateURL(this.PortalSettings.HomeTabId);
+
+                var tc = new TabController();
+                var homeTab = tc.GetTab(this.PortalSettings.HomeTabId, this.PortalSettings.PortalId, false);
+                homeName = homeTab?.LocalizedTabName ?? homeName;
+
+                if (this.UseTitle && homeTab != null && !string.IsNullOrEmpty(homeTab.Title))
+                {
+                    homeName = homeTab.Title;
+                }
+            }
+
+            homeNameEncoded = HttpUtility.HtmlEncode(homeName ?? string.Empty);
+        }
+
+        private string GetTabDisplayName(TabInfo tab)
+        {
+            var tabName = tab.LocalizedTabName;
+            if (this.UseTitle && !string.IsNullOrEmpty(tab.Title))
+            {
+                tabName = tab.Title;
+            }
+
+            return tabName;
+        }
+
+        private string GetTabUrl(TabInfo tab)
+        {
+            var tabUrl = tab.FullUrl;
+
+            if (this.ProfileUserId > -1)
+            {
+                tabUrl = this.navigationManager.NavigateURL(
+                    tab.TabID,
+                    string.Empty,
+                    "UserId=" + this.ProfileUserId.ToString(CultureInfo.InvariantCulture));
+            }
+
+            if (this.GroupId > -1)
+            {
+                tabUrl = this.navigationManager.NavigateURL(
+                    tab.TabID,
+                    string.Empty,
+                    "GroupId=" + this.GroupId.ToString(CultureInfo.InvariantCulture));
+            }
+
+            return tabUrl;
+        }
+
+        private string GetAriaLabel()
+        {
+            // resource key (example):
+            // BreadCrumbAriaLabel.Text = Breadcrumb
+            var localized = Localization.GetString("BreadCrumbAriaLabel.Text", Localization.GetResourceFile(this, MyFileName));
+            return string.IsNullOrWhiteSpace(localized) ? "Breadcrumb" : localized;
         }
 
         private void ResolveSeparatorPaths()
@@ -209,28 +378,29 @@ namespace DotNetNuke.UI.Skins.Controls
                     var url = match.Groups[3].Value;
                     var changed = false;
 
-                    if (url.StartsWith("/"))
+                    if (url.StartsWith("/", StringComparison.Ordinal))
                     {
                         if (!string.IsNullOrEmpty(Globals.ApplicationPath))
                         {
-                            url = string.Format("{0}{1}", Globals.ApplicationPath, url);
+                            url = string.Format(CultureInfo.InvariantCulture, "{0}{1}", Globals.ApplicationPath, url);
                             changed = true;
                         }
                     }
-                    else if (url.StartsWith("~/"))
+                    else if (url.StartsWith("~/", StringComparison.Ordinal))
                     {
                         url = Globals.ResolveUrl(url);
                         changed = true;
                     }
                     else
                     {
-                        url = string.Format("{0}{1}", this.PortalSettings.ActiveTab.SkinPath, url);
+                        url = string.Format(CultureInfo.InvariantCulture, "{0}{1}", this.PortalSettings.ActiveTab.SkinPath, url);
                         changed = true;
                     }
 
                     if (changed)
                     {
                         var newMatch = string.Format(
+                            CultureInfo.InvariantCulture,
                             "{0}={1}{2}{3}",
                             match.Groups[1].Value,
                             match.Groups[2].Value,
